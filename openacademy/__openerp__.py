0a71df849625e4a4f58b438ae593e5c8d4b4e91e
alex <practica565@gmail.com>
2019-05-15 13:22:37 +0200

    Initial commit

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+{
+    'name': "Acadèmia Oberta",
+
+    'summary': """Mòdul per la gestió d'una acadèmia""",
+
+    'description': """
+         Contingut del mòdul:
+            - registrar professors
+            - registrar matèries
+            - gestionar cursos
+    """,
+
+    'author': "Ángel Quintana",
+    'website': "http://www.ies-eugeni.cat",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+	'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo.xml',
+    ],
+}
---
+++ openacademy/models.py
@@ -0,0 +1,45 @@
+﻿# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class Professor(models.Model):
+    _name = 'openacademy.professor'
+
+    professor_nom = fields.Char('nom', size=20, required=True)
+    professor_cognoms = fields.Char('cognoms', size=30, required=True)
+    phone = fields.Char('telèfon', size=9, required=True)
+    email = fields.Char('correuElectrònic')    
+    professor_id = fields.One2many('openacademy.curs', 'professor')
+
+
+class Materia(models.Model):
+    _name = 'openacademy.materia'
+
+    materia_nom = fields.Char('nom', size=20, required=True)
+    descripcio = fields.Text()
+    materia_id = fields.One2many('openacademy.curs', 'materia')
+
+
+class Curs(models.Model):
+    _name = 'openacademy.curs'
+
+    curs_nom = fields.Char(string="nom", size=20, required=True)
+    nivell = fields.Char(string="nivell", size=20)
+    professor = fields.Many2one('openacademy.professor', 'professor_id')
+    materia = fields.Many2one('openacademy.materia', 'materia_id')
+	alumne = fields.Many2one('openacademy.alumne','alumne_id')
+	
+class Alumne(models.Model):
+    _name = 'openacademy.alumne'
+
+    alumne_nom = fields.Char(string="nom", size=20)
+    alumne_cognoms = fields.Char(string="cognoms", size=20)
+    alumne_id = fields.One2many('openacademy.curs', 'alumne')
+	
+    
+	
+	
+	
+
+
